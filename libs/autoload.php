07e205f727cac754879fa477b778e6616158b49a
iranw <iranwing@gmail.com>
2015-09-24 17:56:38 +0800

    update

--- libs/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-spl_autoload_register(function($class){
-    $classfile = str_replace("\\", '/', $class);
-    $classfile = substr($classfile, 2);
-    include $classfile.'.php';
-}, true, true);
